72c854864dd24982be3945ae0af089f62da8b993
lxy2001 <1771478860@qq.com>
2019-06-28 21:40:33 +0800

    io
    io

--- 2/main.cpp
+++ 2/main.cpp
@@ -1,15 +1,38 @@
-//
-//  main.cpp
-//  2
-//
-//  Created by s20181106279 on 2019/6/28.
-//  Copyright © 2019 s20181106279. All rights reserved.
-//
-
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+int main (){
+    int d;
+    cin>>d;
+    double *p=new double [d];
+    for(int i=0;i<d;i++){
+        cin>>p[i];
+    }
+    cout<<"average = ";
+    double sum=0;
+    for(int j=0;j<d;j++){
+        sum=sum+p[j];
+    }
+    double w;
+    w=sum*1.0/d;
+    printf("%.2f",w);
+    cout<<'\n';
+    double max=p[0];
+    for(int i=0;i<d;i++){
+        if(max<p[i]){
+            max=p[i];
+        }
+    }
+    cout<<"max = ";
+    printf("%.2f",max);
+    cout<<'\n';
+    double min=p[0];
+    for(int i=0;i<d;i++){
+        if(min>p[i]){
+            min=p[i];
+        }
+    }
+    cout<<"min = ";
+    printf("%.2f",min);
+    cout<<'\n';
+    delete [] p;
 }
